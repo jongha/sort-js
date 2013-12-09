63c24d80e576988a82e7de7f5d701685f1b24516
Jong-Ha Ahn <jongha.ahn@mrlatte.net>
2013-12-09 16:08:16 +0900

    update testcase

--- test/sort.test.js
+++ test/sort.test.js
@@ -8,16 +8,16 @@
         data2.push(MAX - i);
     }
 
-    var data1_clone = data1.slice(0);;
-    var data2_clone = data2.slice(0);;
+    var data1_clone = data1.slice(0);
+    var data2_clone = data2.slice(0);
     
     test('Bubble sort asc',function() {
         data1.bubble({ order: 'asc' });
-        deepEqual(data1, data1);
+        deepEqual(data1, data1_clone);
     });
 
     test('Bubble sort desc',function() {
         data2.bubble({ order: 'desc' });
-        deepEqual(data2, data2);
+        deepEqual(data2, data2_clone);
     });
 }(jQuery));
